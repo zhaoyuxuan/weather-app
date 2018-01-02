1cff143e084de0bcbe96992955388e6640f238d2
Yuxuan Zhao <yzhao9@ualberta.ca>
2018-01-02 01:05:51 -0700

    testing

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 var map;
 var marker;
+var location = {lat: -25.363, lng: 131.044};
 function initMap(location) {
   
     map = new google.maps.Map(document.getElementById('map'), {
@@ -50,60 +51,60 @@ function getcityoption() {
 }
 
 
-function check() {
 
-    $("#inputcity").autocomplete({
-        source: function(request, response) {
-            var results = $.ui.autocomplete.filter(searchData, request.term);
-            response(results.slice(0, 10));
 
-        },
-        select: function(event, ui) {
-            event.preventDefault();
-            $("#inputcity").val(ui.item.label);
-            var inputid = ui.item.value;
-            var formData = new FormData();
-            formData.set("userInput", inputid);
-            $.ajax({
-                type: "POST",
-                url: "/weatherdata",
-                data: formData,
-                contentType: false,
-                processData: false,
-                success: function(response) {
+$("#inputcity").autocomplete({
+    source: function(request, response) {
+        var results = $.ui.autocomplete.filter(searchData, request.term);
+        response(results.slice(0, 10));
 
-                    let location = {};
+    },
+    select: function(event, ui) {
+        event.preventDefault();
+        $("#inputcity").val(ui.item.label);
+        var inputid = ui.item.value;
+        var formData = new FormData();
+        formData.set("userInput", inputid);
+        $.ajax({
+            type: "POST",
+            url: "/weatherdata",
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(response) {
 
-                    let locationdata = response["city"]["coord"]
+                let location = {};
+
+                let locationdata = response["city"]["coord"]
+          
+                var latlng = new google.maps.LatLng(locationdata["lat"], locationdata["lon"]);
+                marker.setPosition(latlng);
               
-                    var latlng = new google.maps.LatLng(locationdata["lat"], locationdata["lon"]);
-                    marker.setPosition(latlng);
-                  
-                    response = response["list"];
+                response = response["list"];
 
+                $(".infogroup").show();
+                $("#map").show();
+                console.log(response);
+                for (i = 1; i < 6; i++) {
+                    $("#main" + i).text(Math.round(response[i - 1]["main"]["temp"] - 273.15) + "˚C");
+                    $("#windspeed" + i).text("Windspeed: " + response[i - 1]["wind"]["speed"] + "m/s");
+                    $("#humidity" + i).text("Humidity: " + response[i - 1]["main"]["humidity"]);
+                    $("#weatherstate" + i).text(response[i - 1]["weather"][0]["main"]);
+                    var weathercode = response[i - 1]["weather"][0]["id"];
+                    
 
-                    $(".infogroup").show();
-                    $("#map").show();
-                    console.log(response);
-                    for (i = 1; i < 6; i++) {
-                        $("#main" + i).text(Math.round(response[i - 1]["main"]["temp"] - 273.15) + "˚C");
-                        $("#windspeed" + i).text("Windspeed: " + response[i - 1]["wind"]["speed"] + "m/s");
-                        $("#humidity" + i).text("Humidity: " + response[i - 1]["main"]["humidity"]);
-                        $("#weatherstate" + i).text(response[i - 1]["weather"][0]["main"]);
-                        var weathercode = response[i - 1]["weather"][0]["id"];
-                        console.log(weathercode);
-
-                        $("#icon" + i).addClass("wi wi-owm-" + weathercode + " wi-fw");
-                    }
+                    $("#icon" + i).addClass("wi wi-owm-" + weathercode + " wi-fw");
                 }
-            });
-        },
-        focus: function(event, ui) {
-            event.preventDefault();
-            $("#inputcity").val(ui.item.label);
-        }
-    });
-}
+            }
+        });
+    },
+    focus: function(event, ui) {
+        event.preventDefault();
+        $("#inputcity").val(ui.item.label);
+    }
+});
+
+
 
 $.ui.autocomplete.filter = function(array, term) {
     var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(term), "i");
